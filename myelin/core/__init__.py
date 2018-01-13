4e38c75d64221f7d2d3867beb58798f67dadfd11
David Robles <drobles@gmail.com>
2018-01-13 10:49:01 -0800

    Reinforcement learning environment (mdp wrapper)

--- myelin/core/__init__.py
+++ myelin/core/__init__.py
@@ -1,4 +1,5 @@
 from myelin.core.agent import Agent
+from myelin.core.environment import Environment
 from myelin.core.mdp import AlternatingMarkovGame, MarkovGame, MDP
 from myelin.core.policy import Policy
 from myelin.core.value_function import ValueFunction
