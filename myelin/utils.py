84a15d30ea819ccbeb196ca4560c04820c1287fb
David Robles <drobles@gmail.com>
2018-01-13 10:06:47 -0800

    Min/Max q-value helper functions

--- myelin/utils.py
+++ myelin/utils.py
@@ -7,3 +7,15 @@ def check_random_state(seed):
     elif isinstance(seed, np.random.RandomState):
         return seed
     raise ValueError('Seed should be None, int or np.random.RandomState')
+
+
+def max_qvalue(state, actions, qf):
+    if not actions or actions[0] is None:
+        return 0
+    return max([qf[state, action] for action in actions])
+
+
+def min_qvalue(state, actions, qf):
+    if not actions or actions[0] is None:
+        return 0
+    return min([qf[state, action] for action in actions])
