705a9515d03830121fac9459faeaea712ee2e1f5
David Robles <drobles@gmail.com>
2018-01-13 14:19:04 -0800

    Implement Agent interface in QLearning

--- myelin/learners/qlearning.py
+++ myelin/learners/qlearning.py
@@ -1,8 +1,7 @@
-from myelin.mixins import EpisodicLearnerMixin
-from myelin.utils import max_qvalue, min_qvalue
+from myelin.core import Agent
 
 
-class QLearning(EpisodicLearnerMixin):
+class QLearning(Agent):
     """
     Tabular Q-learning.
 
@@ -12,34 +11,36 @@ class QLearning(EpisodicLearnerMixin):
         qfunction: a state-action value function.
         learning_rate: float >= 0.
         discount_factor: float >= 0.
-        selfplay: boolean. Whether to use the same policy
     """
 
-    def __init__(self, env, policy, qfunction, learning_rate=0.1,
-                 discount_factor=1.0, selfplay=False):
-        self.env = env
+    def __init__(self, action_space, policy, qfunction, learning_rate=0.1, discount_factor=1.0, max_value=True):
+        self.action_space = action_space
         self.policy = policy
         self.qfunction = qfunction
         self.learning_rate = learning_rate
         self.discount_factor = discount_factor
-        self.selfplay = selfplay
+        self.max_value = max_value
 
-    def best_qvalue(self, state):
-        if self.selfplay:
-            best_func = max_qvalue if state.cur_player() == 0 else min_qvalue
-            return best_func(state, self.env.get_actions(state), self.qfunction)
-        return max_qvalue(state, self.env.get_actions(state), self.qfunction)
+    #########
+    # Agent #
+    #########
 
-    ###########
-    # Learner #
-    ###########
+    def update(self, experience):
+        """Called after an action has been taken"""
+        state, action, reward, next_state, done = experience
+        if done:
+            target = reward
+        else:
+            next_actions = self.action_space(next_state)
+            q_values = [self.qfunction[next_state, next_action] for next_action in next_actions]
+            best_q_value = max(q_values) if self.max_value else min(q_values)
+            target = reward + (self.discount_factor * best_q_value)
+        td_error = target - self.qfunction[state, action]
+        self.qfunction[state, action] += self.learning_rate * td_error
 
-    def episode(self):
-        while not self.env.is_terminal():
-            state = self.env.get_state()
-            action = self.policy.get_action(state)
-            reward, next_state = self.env.do_action(action)
-            best_qvalue = self.best_qvalue(next_state)
-            target = reward + (self.discount_factor * best_qvalue)
-            td_error = target - self.qfunction[state, action]
-            self.qfunction[state, action] += self.learning_rate * td_error
+    ##########
+    # Policy #
+    ##########
+
+    def get_action(self, state):
+        return self.policy.get_action(state)
