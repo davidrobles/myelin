37b21282bbb629767b58e068dbfa6bd01b9439ba
David Robles <drobles@gmail.com>
2018-01-13 17:02:31 -0800

    MDP Environment

--- myelin/core/environment.py
+++ myelin/core/environment.py
@@ -4,13 +4,43 @@ import abc
 class Environment(object):
     """An environment for reinforcement learning interactions."""
 
+    @property
+    def action_space(self):
+        pass
+
+    @abc.abstractmethod
+    def get_actions(self, state):
+        """Returns the available actions in the given state."""
+
+    @abc.abstractmethod
+    def get_state(self):
+        """Returns the current state."""
+
+    @abc.abstractmethod
+    def do_action(self, action):
+        """
+        Performs the given action in the current state.
+        Returns (reward, next_state).
+        """
+
+    def is_terminal(self):
+        return self._mdp.is_terminal(self.get_state())
+
+    @abc.abstractmethod
+    def reset(self):
+        """Resets the current state to the start state."""
+
+
+class MDPEnvironment(object):
+    """An environment for reinforcement learning interactions."""
+
     def __init__(self, mdp):
         self._mdp = mdp
         self._cur_state = self._mdp.start_state()
 
     @property
     def action_space(self):
-        return self._mdp.actions
+        return self._mdp.get_actions
 
     @abc.abstractmethod
     def get_actions(self, state):
