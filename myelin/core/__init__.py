4ee37113a475d67d34816bc48a87118df6bb6fc8
David Robles <drobles@gmail.com>
2018-01-14 13:43:34 -0800

    Add EnvironmentInteraction to core module imports

--- myelin/core/__init__.py
+++ myelin/core/__init__.py
@@ -1,5 +1,6 @@
 from myelin.core.agent import Agent
 from myelin.core.environment import Environment
+from myelin.core.interactions import EnvironmentInteraction
 from myelin.core.mdp import AlternatingMarkovGame, MarkovGame, MDP
 from myelin.core.mdp_environment import MDPEnvironment
 from myelin.core.policy import Policy
