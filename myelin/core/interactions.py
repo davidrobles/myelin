9f3fce11f1519feb7a37151f21f2cd8d3a44b56f
David Robles <drobles@gmail.com>
2018-01-14 18:23:17 -0800

    Simplify interactions as RLInteraction

--- myelin/core/interactions.py
+++ myelin/core/interactions.py
@@ -1,34 +1,23 @@
-from abc import abstractmethod
-
 from myelin.utils import Experience
 from myelin.utils.callbacks import CallbackList
 
 
-class EpisodicInteraction:
-
-    def train(self, n_episodes, callbacks=None):
-        """Trains the model for a fixed number of episodes."""
-        # callbacks = CallbackList(callbacks)
-        # callbacks.on_train_begin()
-        for episode in range(n_episodes):
-            # callbacks.on_episode_begin(episode)
-            # self.env.reset()
-            # print('Episode {}'.format(episode))
-            self.episode()
-            # callbacks.on_episode_end(episode)
-        # callbacks.on_train_end()
-
-    @abstractmethod
-    def episode(self):
-        pass
-
-
-class EnvironmentInteraction(EpisodicInteraction):
-    """Interaction between an Environment and an agent"""
+class RLInteraction:
+    """An episodic interaction between an agent and an environment."""
 
     def __init__(self, env, agent):
         self.env = env
         self.agent = agent
+
+    def train(self, n_episodes, callbacks=None):
+        """Trains the model for a fixed number of episodes."""
+        callbacks = CallbackList(callbacks)
+        callbacks.on_train_begin()
+        for episode in range(n_episodes):
+            callbacks.on_episode_begin(episode)
+            self.episode()
+            callbacks.on_episode_end(episode)
+        callbacks.on_train_end()
 
     def episode(self):
         self.env.reset()
