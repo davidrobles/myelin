64cdcbabdb1b497ad63b994dd86e54217f1b2484
David Robles <drobles@gmail.com>
2018-04-01 09:17:44 -0700

    Style fix

--- myelin/policies/greedy.py
+++ myelin/policies/greedy.py
@@ -27,8 +27,8 @@ class Greedy(Policy):
         if action not in actions:
             raise ValueError('Invalid action')
         state_actions = [(state, action) for action in actions]
-        _, best_action = max(state_actions, key=lambda state_action: self.qfunc[state_action])
-        best_value = self.qfunc[state, best_action]
+        _, first_best_action = max(state_actions, key=lambda state_action: self.qfunc[state_action])
+        best_value = self.qfunc[state, first_best_action]
         best_actions = [action for state, action in state_actions if self.qfunc[state, action] == best_value]
         if action not in best_actions:
             return 0
