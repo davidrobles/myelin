754e5ae5669e3b62d127fdbbb1c335656a479226
David Robles <drobles@gmail.com>
2018-05-19 10:31:47 -0700

    Add test_should_return_zero_for_non_existent_action_values

--- tests/test_tabular_vf.py
+++ tests/test_tabular_vf.py
@@ -5,7 +5,7 @@ from myelin.value_functions.tabular import TabularVF
 
 class TestTabularVF(unittest.TestCase):
     def setUp(self):
-        self.vf = TabularVF(init=False)
+        self.vf = TabularVF(init=True)
 
     def test_save_and_retrieve_state_values(self):
         state = 1
@@ -18,9 +18,19 @@ class TestTabularVF(unittest.TestCase):
         self.vf[state, action] = 10
         self.assertEqual(self.vf[state, action], 10)
 
-    def test_should_return_zero_for_non_existent_values(self):
+    def test_should_return_zero_for_non_existent_state_values(self):
         vf = TabularVF(init=False)
-        self.assertEqual(vf[0], 0)
+        state = 0
+        self.assertEqual(vf[state], 0)
+        state = 1
+        self.assertEqual(vf[state], 0)
+
+    def test_should_return_zero_for_non_existent_action_values(self):
+        vf = TabularVF(init=False)
+        state, action = 0, 0
+        self.assertEqual(vf[state, action], 0)
+        state, action = 1, 1
+        self.assertEqual(vf[state, action], 0)
 
     def test_should_return_randomly_initialized_values_for_non_existent_values(self):
         from unittest.mock import MagicMock
