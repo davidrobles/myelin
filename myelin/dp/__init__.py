27892ed8b63b9a7fdd19e8aafecc9c55f65f785c
David Robles <drobles@gmail.com>
2021-09-11 13:56:00 -0700

    Add Value Iteration callbacks

--- myelin/dp/__init__.py
+++ myelin/dp/__init__.py
@@ -1,2 +1,3 @@
 from myelin.dp.value_iteration import ValueIteration
 from myelin.dp.policy_iteration import PolicyIteration
+from myelin.dp.value_iteration import ValueIterationCallback
--- myelin/dp/value_iteration.py
+++ myelin/dp/value_iteration.py
@@ -1,14 +1,37 @@
+
+class ValueIterationCallback:
+    def on_iteration_begin(self):
+        """Called at the beginning of every iteration (sweep)."""
+
+    def on_iteration_end(self,):
+        """Called at the end of every iteration (sweep)."""
+
+
+class ValueIterationCallbackList:
+    def __init__(self, callbacks=None):
+        self.callbacks = callbacks or []
+
+    def on_iteration_begin(self):
+        for callback in self.callbacks:
+            callback.on_iteration_begin()
+
+    def on_iteration_end(self):
+        for callback in self.callbacks:
+            callback.on_iteration_end()
+
+
 class ValueIteration:
     """
     Value iteration (Bellman 1957), also called backward induction, is an algorithm that computes the values for all
     the states in the markov decision process. In value iteration no policy is used.
     """
 
-    def __init__(self, mdp, theta, gamma, vfunction):
+    def __init__(self, mdp, theta, gamma, vfunction, callbacks=None):
         self.mdp = mdp
         self.theta = theta
         self.gamma = gamma
         self.vf = vfunction
+        self.callbacks = ValueIterationCallbackList(callbacks)
 
     def iteration(self):
         delta = 0.0
@@ -33,6 +56,8 @@ class ValueIteration:
         print('Value Iteration started...')
         delta = 1000000
         while delta >= self.theta:
+            self.callbacks.on_iteration_begin()
             delta = self.iteration()
             print('Delta: %.4f' % (delta))
+            self.callbacks.on_iteration_end()
         print('DP Value Iteration finished!')
